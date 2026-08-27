519b40b033e01941361c430a6dbaf5aa82c7980c
YuukiLucero <meyklucero@gmail.com>
2026-08-27 09:45:02 +0800

    Added query for return

--- backup/lab3.sql
+++ backup/lab3.sql
@@ -57,3 +57,36 @@ INSERT INTO borrow (student_id, book_id) VALUES
 
     WHERE br.borrow_return_date IS NULL
     ORDER  BY br.borrow_date DESC;
+
+    UPDATE borrow
+    SET borrow_return_date = CURRENT_TIMESTAMP
+    WHERE borrow_id = 7 AND borrow_return_date IS NULL;
+
+    UPDATE borrow
+    SET borrow_return_date = CURRENT_TIMESTAMP
+    WHERE borrow_id = 8 AND borrow_return_date IS NULL;
+
+
+    SELECT 
+        br.borrow_id,
+
+        CONCAT(
+           s.student_first_name,
+           ' ',
+           s.student_last_name
+        ) as student_name,
+
+        b.book_title,
+        b.book_author,
+        b.book_category,
+
+        br.borrow_date,
+        br.borrow_return_date
+    FROM borrow br
+    JOIN students s ON
+        br.student_id = s.student_id
+    JOIN books b ON
+        br.book_id = b.book_id
+
+    WHERE br.borrow_return_date IS NOT NULL
+    ORDER BY br.borrow_return_date DESC;
