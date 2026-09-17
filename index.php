f31d7ab00f46057cf856443bdf0933ad60bdc639
YuukiLucero <meyklucero@gmail.com>
2026-09-17 10:23:42 +0800

    Added foreach in students section

--- index.php
+++ index.php
@@ -31,6 +31,20 @@ $section = $_GET['section'] ?? 'students';
 // CRUD Operations
 $actions = $_GET['action'] ?? '';
 
+//------------------------------------------------------------
+// Students 
+//------------------------------------------------------------
+
+// Fetch students
+if($section === 'students') {
+    $stmt = $pdo->query("
+    SELECT *
+    FROM students
+    ORDER BY student_id DESC
+    ");
+    $students = $stmt->fetchAll();
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -38,7 +52,7 @@ $actions = $_GET['action'] ?? '';
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-    <head>
+    </head>
     <body>
         <h1>Simple Library System</h1>
         <nav>
@@ -47,6 +61,41 @@ $actions = $_GET['action'] ?? '';
             <a href="index.php?section=borrow">Borrow</a>
         </nav>
         <hr>
+
+        <?php if($section === 'students'): ?>
+            <h1>Students</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Course</th>
+                    <th>Created At</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($students as $student): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($student['student_id']); ?></td>
+                        <td><?= htmlspecialchars($student['student_first_name']); ?></td>
+                        <td><?= htmlspecialchars($student['student_last_name']); ?></td>
+                        <td><?= htmlspecialchars($student['student_course']); ?></td>
+                        <td><?= htmlspecialchars($student['student_created_at']); ?></td>
+                        
+                        <td>
+                            <a></a>Edit</a>
+                            |
+                            <a>Delete</a>
+                        </td>
+
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <?php endif; ?>
+
         <?php if($section === 'students'): ?>
             <h1>Students</h1>
         <?php endif; ?>
